e8489bda4b5031222567f0b92fd87ff208cfb5c6
pr1yash <pjshah2019@gmail.com>
2025-02-12 18:12:17 -0600

    Login Registration Buttons

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -20,7 +20,7 @@ class LoginScreen extends StatelessWidget {
         context: context,
         barrierDismissible: false,
         builder: (context) {
-          return Center(child: CircularProgressIndicator()); // Loading Indicator
+          return Center(child: CircularProgressIndicator());
         },
       );
 
@@ -46,94 +46,94 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true, // Allows resizing when keyboard appears
-      body: SafeArea( // Prevents overlap with system UI
-        child: SingleChildScrollView( // Makes the screen scrollable
-          child: Container(
-            color: Colors.white, // Plain white background
-            height: MediaQuery.of(context).size.height, // Maintain full height
-            child: Column(
-              children: [
-                Expanded(
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset('assets/eco_eagle_logo.png', height: 150),
-                          Text("Welcome Back!",
-                              style: TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black)),
-                          SizedBox(height: 20),
-                          TextField(
-                            controller: emailController,
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Color(0x1A00853E), // 10% opacity of #00853E
-                              prefixIcon: Icon(Icons.person,
-                                  color: Color.fromARGB(255, 47, 48, 47)),
-                              hintText: "Email",
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10), // Rounded corners
-                                borderSide: BorderSide(color: Color(0xFF00853E)),
-                              ),
-                            ),
+      resizeToAvoidBottomInset: true, // Prevents UI from getting cut off by keyboard
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/eco_eagle_logo.png', height: 150),
+                      Text("Welcome Back!",
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black)),
+                      SizedBox(height: 20),
+                      TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color(0x1A00853E),
+                          prefixIcon: Icon(Icons.email, color: Colors.grey),
+                          hintText: "Email",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
                           ),
-                          SizedBox(height: 10),
-                          TextField(
-                            controller: passwordController,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Color(0x1A00853E), // 10% opacity of #00853E
-                              prefixIcon: Icon(Icons.lock,
-                                  color: Color.fromARGB(255, 47, 48, 47)),
-                              hintText: "Password",
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10), // Rounded corners
-                                borderSide: BorderSide(color: Color(0xFF00853E)),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          ElevatedButton(
-                            onPressed: () => _signIn(context),
-                            child: Text("SIGN IN",
-                                style: TextStyle(color: Colors.white)),
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Color(0xFF00853E), // Green button
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10), // Rounded corners
-                              ),
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 40, vertical: 15), // Adjust size
-                            ),
-                          ),
-                          TextButton(
-                              onPressed: () {},
-                              child: Text("Forgot Password?",
-                                  style: TextStyle(
-                                      color: const Color.fromARGB(
-                                          255, 32, 127, 236)))),
-                        ],
+                        ),
                       ),
-                    ),
+                      SizedBox(height: 10),
+                      TextField(
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color(0x1A00853E),
+                          prefixIcon: Icon(Icons.lock, color: Colors.grey),
+                          hintText: "Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                        onPressed: () => _signIn(context),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF00853E),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 40, vertical: 15),
+                        ),
+                        child: Text("SIGN IN",
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                      ),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text("Forgot Password?",
+                              style: TextStyle(color: Colors.blue))),
+                      SizedBox(height: 10),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/register');
+                        },
+                        child: Text("Create an Account",
+                            style: TextStyle(color: Colors.green, fontSize: 16)),
+                      ),
+                    ],
                   ),
                 ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Image.asset(
-                    'assets/trees.png',
-                    width: double.infinity, // Full width
-                    fit: BoxFit.cover, // Cover the width
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset(
+                'assets/trees.png',
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
         ),
       ),
     );
